0cc7763a6b8f8359c43759edd70f50fc6b0f1f26
Tom von Schwerdtner <tomvons@gmail.com>
2017-02-20 13:34:42 -0500

    Add optional token support (#4)
    This is useful in a development setup (such as minikube).

--- assets/common.sh
+++ assets/common.sh
@@ -10,27 +10,35 @@ setup_kubernetes() {
     exit 1
   fi
   if [[ "$cluster_url" =~ https.* ]]; then
+
     cluster_ca=$(jq -r '.source.cluster_ca // ""' < $payload)
     admin_key=$(jq -r '.source.admin_key // ""' < $payload)
     admin_cert=$(jq -r '.source.admin_cert // ""' < $payload)
+    token=$(jq -r '.source.token // ""' < $payload)
 
     mkdir -p /root/.kube
-    ca_path="/root/.kube/ca.pem"
-    key_path="/root/.kube/key.pem"
-    cert_path="/root/.kube/cert.pem"
-    echo "$cluster_ca" | base64 -d > $ca_path
-    echo "$admin_key" | base64 -d > $key_path
-    echo "$admin_cert" | base64 -d > $cert_path
 
+    ca_path="/root/.kube/ca.pem"
+    echo "$cluster_ca" | base64 -d > $ca_path
     kubectl config set-cluster default --server=$cluster_url --certificate-authority=$ca_path
-    kubectl config set-credentials admin --client-key=$key_path --client-certificate=$cert_path
+
+    if [ ! -z "$token" ]; then
+      kubectl config set-credentials admin --token=$token
+    else
+      key_path="/root/.kube/key.pem"
+      cert_path="/root/.kube/cert.pem"
+      echo "$admin_key" | base64 -d > $key_path
+      echo "$admin_cert" | base64 -d > $cert_path
+      kubectl config set-credentials admin --client-certificate=$cert_path --client-key=$key_path
+    fi
+
     kubectl config set-context default --cluster=default --user=admin
   else
     kubectl config set-cluster default --server=$cluster_url
     kubectl config set-context default --cluster=default
   fi
-  kubectl config use-context default
 
+  kubectl config use-context default
   kubectl cluster-info
 }
 
